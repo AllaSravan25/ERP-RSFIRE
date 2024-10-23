96712b744a64c7ac4a5562962c5d9c130dc8dc65
Alla Sravan <sravan4g@gmail.com>
2024-10-23 08:12:36 +0530

    with login page and vercel file for vercel hosting

--- server/index.js
+++ server/index.js
@@ -22,6 +22,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const port = process.env.PORT || 8038;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 
 
 // ----------------------- Notifications -----------------------
